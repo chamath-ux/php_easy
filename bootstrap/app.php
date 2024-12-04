0fea0715ad8cf484c49b07383e9d894434013220
rathnayake <rathnayake@itplace.com>
2024-12-04 15:25:59 +0530

    adding table to modal

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,10 +3,12 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../routes/web.php';
 
+use Support\Database\Model;
 
 use Dotenv\Dotenv;
 // Load .env file
 $dotenv = Dotenv::createImmutable(__DIR__.'/../') ;
 $dotenv->load();
+Model::setConnection();
 
  
--- index.php
+++ index.php
@@ -8,15 +8,7 @@ use App\Http\TestController;
 $test = new TestController();
 
 $middleware = new AuthMiddleware;
-$request = [
-    'url' => '/home',
-    'method' => 'GET',
-    'user' => 'John Doe', // Comment this to simulate an unauthorized request
-];
 
-print_r($middleware->handle($request, function($request){
-    return "Final response: " . json_encode($request);
-}));
 echo"<pre>";
 print_r($test->test(1));
 echo"</pre>"; 
